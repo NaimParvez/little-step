c902d73703d6d012433b10a295c1c8bd272b1d80
Rubait-islam-jami <146682245+Rubait-islam-jami@users.noreply.github.com>
2024-08-24 18:16:15 +0600

    Your commit message here

--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
+
+class Home(generic.TemplateView):
+    template_name ='home.html'
 
 # Create your views here.
+
+class ProductDetails(generic.TemplateView):
+    template_name = 'product/product-details.html'
