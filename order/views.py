21b2a257f78ef4a82d518309e244c73b2b0c315f
NaimParvez <parveznaim0@gmail.com>
2024-09-16 12:15:39 +0600

    checkout form added( info process pending)

--- order/views.py
+++ order/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render,redirect
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 from .forms import CheckoutForm
 
 from .models import Order,OrderItem
 from cart.carts import Cart
 
-class Checkout(generic.View):
+class Checkout(LoginRequiredMixin,generic.View):
+    login_url = reverse_lazy('login')
     def get(self,*args,**kwargs):
         # order =Order.objects.get(user=self.request.user,paid=False)
         form =CheckoutForm()
